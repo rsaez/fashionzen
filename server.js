24697fabc320b00d3f47b9f8d1e4031a0e9487e6
Rounder1 <tolerdavid@gmail.com>
2018-07-06 13:32:00 -0400

    first commit, server file setup

--- server.js
+++ server.js
@@ -8,15 +8,22 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// Serve up static assets (usually on heroku)
+// Serve up static assets for production deployment (On Heroku in this case)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
 
+// Set up promises with mongoose
+mongoose.Promise = global.Promise;
+
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
+  {
+    useMongoClient: true
+  }
+);
 
 // Start the API server
 app.listen(PORT, function() {
