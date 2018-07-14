aadd6c771d1151b381e69079ce3dfc95b3f7d332
Rounder1 <tolerdavid@gmail.com>
2018-07-14 11:03:06 -0400

    fixed backend I think

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,68 +1,12 @@
 const db = require("../models");
-const User = require("../models/User.js");
 
+// Defining methods for the UserController
 module.exports = {
 
-  /*
-   * Sign up ops
-   */
-  signUp: function(req, res) {
-    const { body } = req;
-    const {
-      password
-    } = body;
-    let {
-      email
-    } = body;
-
-    if (!email) {
-      return res.send({
-        success: false,
-        message: 'Error: Email cannot be blank.'
-      });
-    }
-    if (!password) {
-      return res.send({
-        success: false,
-        message: 'Error: Password cannot be blank.'
-      });
-    }
-    email = email.toLowerCase();
-    email = email.trim();
-    // Steps:
-    // 1. Verify email doesn't exist
-    // 2. Save
-    User.find({
-      email: email
-    }, (err, previousUsers) => {
-      if (err) {
-        return res.send({
-          success: false,
-          message: 'Error: Server error'
-        });
-      } else if (previousUsers.length > 0) {
-        return res.send({
-          success: false,
-          message: 'Error: Account already exist.'
-        });
-      }
-      // Save the new user
-      const newUser = new User();
-      newUser.email = email;
-      newUser.password = newUser.generateHash(password);
-      newUser.save((err, user) => {
-        if (err) {
-          return res.send({
-            success: false,
-            message: 'Error: Server error'
-          });
-        }
-        return res.send({
-          success: true,
-          message: 'Signed up'
-        });
-      });
-    });
-  } // end of sign up
+  create: function(req, res) {
+    db.User.create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
 
 };
--- routes/api/User.js
+++ routes/api/User.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const userController = require("../../controllers/userController");
 
-// Matches with "/api/user/
-router
+// Matches with "/api/User/
+router.route("/")
 .post(userController.create);
 
 module.exports = router;
