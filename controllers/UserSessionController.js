3aa5512152d7a9b362828e0d220b6c4c6650cb09
Rodolfo Saez <rolo928@hotmail.com>
2018-07-14 13:03:09 -0400

    added sigin post findONE

--- controllers/UserSessionController.js
+++ controllers/UserSessionController.js
@@ -4,64 +4,60 @@ const db = require("../models");
 module.exports = {
 
   findOne: function(req, res) {
-    console.log(req);
-    const { body } = req;
-    const {
-      password
-    } = body;
-    let {
-      email
-    } = body;
+    console.log("======================================================");
+    console.log(req.body);
+    let email = req.body.email
+    let password = req.body.password
 
-    if (!email) {
-      return res.send({
-        success: false,
-        message: 'Error: Email cannot be blank.'
-      });
-    }
-    if (!password) {
-      return res.send({
-        success: false,
-        message: 'Error: Password cannot be blank.'
-      });
-    }
     email = email.toLowerCase();
     email = email.trim();
-    // Steps:
-    // 1. Verify email doesn't exist
-    // 2. Save
-    User.find({
-      email: email
-    }, (err, previousUsers) => {
+
+    // run a find mongoose method that looks for an email in the user collection that matches what is passed
+    // by the api call
+    db.User.find( {email: email}, (err, users) => {
+
+      //check error
       if (err) {
+        console.log('err 2:', err);
         return res.send({
           success: false,
-          message: 'Error: Server error'
-        });
-      } else if (previousUsers.length > 0) {
-        return res.send({
-          success: false,
-          message: 'Error: Account already exist.'
+          message: 'Error: server error'
         });
       }
-      // Save the new user
-      const newUser = new User();
-      newUser.email = email;
-      newUser.password = newUser.generateHash(password);
-      newUser.save((err, user) => {
+
+      //check error
+      if (users.length != 1) {
+        return res.send({
+          success: false,
+          message: 'Error: Invalid'
+        });
+      }
+
+      //set the first user returned by the .find method equal to "user"
+      const user = users[0];
+
+      // Otherwise correct user!!!!!
+      const userSession = new db.UserSession();
+      userSession.userId = user._id;
+
+      userSession.save((err, doc) => {
         if (err) {
+          console.log(err);
           return res.send({
             success: false,
-            message: 'Error: Server error'
+            message: 'Error: server error'
           });
         }
+
         return res.send({
           success: true,
-          message: 'Signed up'
-        });
-      });
-    });
+          message: 'Valid sign in',
+          token: doc._id
+        }); // end of return
 
-  }
+      });  //end of User Session.save
+    }); //end of user find
 
-};
+  } //end of findOne
+
+};  //end of module exports
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const closetRoutes = require("./closet");
 const userRoutes = require("./signup");
+const userSessionRoutes = require("./signin");
 
 // add the closet routes and add the "/closet" to the url
 router.use("/closet", closetRoutes);
 router.use("/signup", userRoutes);
+router.use("/signin", userSessionRoutes);
 
 module.exports = router;
