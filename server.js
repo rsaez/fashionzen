27723f1a76c65a0e0efb577d33e8eeafac1189e7
Rodolfo Saez <rolo928@hotmail.com>
2018-07-21 11:55:48 -0400

    adjusted server port from 3000 to 3001

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(routes);
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fashionzen");
 
 // Start the API server
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3001, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 // app.listen(PORT, function() {
